2650056ce4f42458f66ca54334045607d3a9512b
Markos96 <markossolari@gmail.com>
2021-05-23 15:54:26 -0300

    Cambios realizados

--- src/main/java/com/utn/teamA/App.java
+++ src/main/java/com/utn/teamA/App.java
@@ -6,7 +6,5 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        Marcos a = new Marcos("Marcos");
-        a.saludar();
     }
 }
