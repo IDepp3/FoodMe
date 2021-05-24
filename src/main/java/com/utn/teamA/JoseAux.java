8ca27e403c018dc769a34082e8598c7aaea283ab
Jose Alvarez U <jsemalvarez@gmail.com>
2021-05-23 22:46:23 -0300

    clase ingrediente

--- src/main/java/com/utn/teamA/JoseAux.java
+++ src/main/java/com/utn/teamA/JoseAux.java
@@ -1,8 +1,34 @@
 package com.utn.teamA;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class JoseAux {
 
+    private List<Ingrediente> listaDeIngredites = new ArrayList<>();
+
     public void init(){
-        System.out.println("Holaaaaaa");
+
+        Scanner teclado = new Scanner(System.in);
+        char otroIngrediente = 's';
+
+        while ( otroIngrediente == 's' ){
+            agregarIngrediente();
+
+            System.out.println("Desea agregar otro ingrediente? [ S/N ] ");
+            otroIngrediente = teclado.next().charAt(0);
+
+        }
+
+    }
+
+    public void agregarIngrediente(){
+
+        Ingrediente nuevoIngrediente = new Ingrediente();
+
+        nuevoIngrediente.crear();
+
+        listaDeIngredites.add(nuevoIngrediente);
     }
 }
