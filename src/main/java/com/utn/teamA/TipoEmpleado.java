eaea34883638c514fc1c36b61481f7a3b60d1a64
Jose Alvarez U <jsemalvarez@gmail.com>
2021-05-24 22:49:10 -0300

    recuperando clases

---
+++ src/main/java/com/utn/teamA/TipoEmpleado.java
@@ -0,0 +1,19 @@
+package com.utn.teamA;
+
+public enum TipoEmpleado {
+    MOZO("mozo",1500),
+    BARTENDER("bartender",2200),
+    SUSHIMAN("sushiman",3610);
+
+    protected final int sueldo;
+    protected final String tipo;
+    private TipoEmpleado(String tipo, int sueldo) {
+        this.tipo = tipo;
+        this.sueldo = sueldo;
+        
+    }
+
+    
+
+    
+}
