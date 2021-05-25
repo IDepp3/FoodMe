cf7be7d1587c95194bd9a0de6857319af9633375
Markos96 <markossolari@gmail.com>
2021-05-25 00:28:07 -0300

    Creacion de la clase Cliente.java con List<Reservas>

--- src/main/java/com/utn/teamA/Cliente.java
+++ src/main/java/com/utn/teamA/Cliente.java
@@ -1,9 +1,44 @@
 package com.utn.teamA;
 
+import java.time.LocalDate;
 import java.util.List;
 
-public class Cliente {
+import com.utn.teamA.Usuario;
 
-    private List<> reservas;
+public class Cliente extends Usuario {
+
+    private List<Reserva> reservas;
+
+    public Cliente(List<Reserva> reservas) {
+        this.reservas = reservas;
+    }
+
+    public Cliente(int id, String nombre, String apellido, LocalDate Nacimiento, String telefono, String direccion,
+            String dni, String email, int idUsuario, String nombreUsuario, String contraseña, LocalDate fechaRegistro,
+            List<Reserva> reservas) {
+        super(id, nombre, apellido, Nacimiento, telefono, direccion, dni, email, idUsuario, nombreUsuario, contraseña,
+                fechaRegistro);
+        this.reservas = reservas;
+    }
+
+    public Cliente() {
+
+    }
+
+    public List<Reserva> getReservas() {
+        return reservas;
+    }
+
+    public void setReservas(List<Reserva> reservas) {
+        this.reservas = reservas;
+    }
+
+    @Override
+    public String toString() {
+        return " Cliente | Reservas: " + reservas;
+    }
+
+    
+
 
 }
