7fceea9aa70badc4b9e6844af17d8c05af8c8208
Markos96 <markossolari@gmail.com>
2021-05-26 20:38:24 -0300

    Trabajando las clases con Json

--- src/main/java/com/utn/teamA/app/App.java
+++ src/main/java/com/utn/teamA/app/App.java
@@ -1,11 +1,23 @@
 package com.utn.teamA;
 
+import com.utn.teamA.clases.Vista;
+import com.utn.teamA.clases.User;
 
 public class App {
     
     public static void main(String[] args) {
 
         System.out.println("Hola Mundo");
+        
+        Vista v = new Vista ();
+
+        v.menuPrincipal();
+
+        v.seleccionOpcion();
+
+        User u = new User();
+
+        u.login();
 
     }
 }
