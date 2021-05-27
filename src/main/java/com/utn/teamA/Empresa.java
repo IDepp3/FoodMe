0968fcba1bab8e478c372894514ac19ad8b0da1e
IDepp3 <alvarez.joaquinez@gmail.com>
2021-05-27 10:23:13 -0300

    Creamos clase Empresa y Vista

---
+++ src/main/java/com/utn/teamA/Empresa.java
@@ -0,0 +1,20 @@
+package com.utn.teamA;
+
+public class Empresa {
+
+    // region ATRIBUTOS
+
+    private Vista vista;
+
+    // endregion
+
+    // region CONSTRUCTORES
+
+    public Empresa(){
+        this.vista = new Vista();
+    }
+   
+    // endregion
+
+    
+}
---
+++ src/main/java/com/utn/teamA/Vista.java
@@ -0,0 +1,20 @@
+package com.utn.teamA;
+
+import java.util.Scanner;
+
+public class Vista {
+
+    // region ATRIBUTOS
+
+    private Scanner scanner;
+
+    // endregion
+
+    // region METODOS
+
+    public void menuClientePrincipal(){
+        System.out.println("MENU PRINCIPAL");
+        System.out.println();
+        System.out.println();
+    }
+}
