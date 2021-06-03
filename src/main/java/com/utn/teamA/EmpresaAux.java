894c9dc93eade5e71bf196ee54e38929a8c64223
Jose Alvarez U <jsemalvarez@gmail.com>
2021-06-02 22:09:45 -0300

    menu Plato

--- src/main/java/com/utn/teamA/EmpresaAux.java
+++ src/main/java/com/utn/teamA/EmpresaAux.java
@@ -8,10 +8,19 @@ public class EmpresaAux {
 
     //TODO ver donde va la lista de ingredientes Map o List
     private List<Ingrediente> listaDeIngredites = new ArrayList<>();
+    private List<Plato> listaDePlatos = new ArrayList<>();
 
     public void init(){
 
-        ingredienteMenu();
+        //ingredienteMenu();
+        platoMenu();
+
+        //listaDeIngredites.add(new Ingrediente("tomate","tomate",50,"proveedor"));
+        //listaDeIngredites.add(new Ingrediente("papa","papa",50,"proveedor"));
+
+        //Plato plato1 = new Plato("Ensalada",listaDeIngredites,"ensalada",TipoPlato.ENTRADA);
+
+        //System.out.println(plato1);
 
     }
 
@@ -51,7 +60,9 @@ public class EmpresaAux {
                     System.out.println(Color.ANSI_BLACK + "Seccion en construccion" + Color.ANSI_RESET);
                     break;
                 case 4:
-                    System.out.println("Listar");
+                    for ( Ingrediente ingrediente : listaDeIngredites ) {
+                        System.out.println(ingrediente);
+                    }
                     break;
                 default:
                     System.out.println(
@@ -70,9 +81,9 @@ public class EmpresaAux {
     //region agregar ingrediente
     public void agregarIngredienteView(){
 
-        char otroIngrediente = 's';
+        char unElementoMas = 's';
 
-        while ( otroIngrediente == 's' ){
+        while ( unElementoMas == 's' ){
             agregarIngrediente();
 
             System.out.print(
@@ -82,7 +93,7 @@ public class EmpresaAux {
                 Color.ANSI_YELLOW + "/" +
                 Color.ANSI_RED + " N " +
                 Color.ANSI_YELLOW + "]" + Color.ANSI_RESET);
-            otroIngrediente = Helpers.charAt0();
+            unElementoMas = Helpers.charAt0();
 
 
         }
@@ -96,6 +107,94 @@ public class EmpresaAux {
         nuevoIngrediente.crear();
 
         listaDeIngredites.add(nuevoIngrediente);
+    }
+
+    //endregion
+
+
+    public void platoMenu(){
+        int opcion = 0;
+        boolean continuar = true;
+
+        do{
+            System.out.println(
+                    Color.ANSI_BLUE +
+                            "\n\n" +
+                            " ------------------------------------\n" +
+                            "| \t\t M E N U    P L A T O \t\t |\n" +
+                            " ------------------------------------" +
+                            " \n\n" +
+                            Color.ANSI_RESET);
+            System.out.println(Color.ANSI_YELLOW + "\t Ingrese una opcion:" + Color.ANSI_RESET);
+            System.out.println(Color.ANSI_GREEN + " 1 " + Color.ANSI_RESET + " Nuevo Plato");
+            System.out.println(Color.ANSI_GREEN + " 2 " + Color.ANSI_RESET + " Editar Plato");
+            System.out.println(Color.ANSI_GREEN + " 3 " + Color.ANSI_RESET + " Eliminar Plato");
+            System.out.println(Color.ANSI_GREEN + " 4 " + Color.ANSI_RESET + " Listar Platos");
+            System.out.println(Color.ANSI_RED + " 0 " + Color.ANSI_RESET + " Volver");
+
+            opcion = Helpers.validarInt();
+
+            switch (opcion){
+                case 0:
+                    continuar = false;
+                    break;
+                case 1:
+                    agregarPlatoView();
+                    break;
+                case 2:
+                    System.out.println(Color.ANSI_BLACK + "Seccion en construccion" + Color.ANSI_RESET);
+                    break;
+                case 3:
+                    System.out.println(Color.ANSI_BLACK + "Seccion en construccion" + Color.ANSI_RESET);
+                    break;
+                case 4:
+                    for ( Plato plato : listaDePlatos ) {
+                        System.out.println(plato);
+                    }
+                    break;
+                default:
+                    System.out.println(
+                            Color.ANSI_RED +
+                                    "\n\t [[ " + opcion + " ]] NO ES UNA OPCION VALIDA \n" +
+                                    Color.ANSI_RESET
+                    );
+                    break;
+
+            }
+
+        } while( continuar );
+    }
+
+
+    //region agregar plato
+    public void agregarPlatoView(){
+
+        char unElementoMas = 's';
+
+        while ( unElementoMas == 's' ){
+            agregarPlato();
+
+            System.out.print(
+                    Color.ANSI_YELLOW + " \n\n \t\t Desea agregar otro plato? " +
+                            Color.ANSI_YELLOW + "[" +
+                            Color.ANSI_GREEN + " S " +
+                            Color.ANSI_YELLOW + "/" +
+                            Color.ANSI_RED + " N " +
+                            Color.ANSI_YELLOW + "]" + Color.ANSI_RESET);
+            unElementoMas = Helpers.charAt0();
+
+
+        }
+
+    }
+
+    public void agregarPlato(){
+
+        Plato nuevoPlato = new Plato();
+
+        nuevoPlato.crear();
+
+        listaDePlatos.add(nuevoPlato);
     }
 
     //endregion
