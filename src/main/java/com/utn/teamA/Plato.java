7683e00ec4d11499bcdf4076bd3c9a8933fa12ea
Jose Alvarez U <jsemalvarez@gmail.com>
2021-06-02 22:01:47 -0300

    crear plato

--- src/main/java/com/utn/teamA/Plato.java
+++ src/main/java/com/utn/teamA/Plato.java
@@ -1,6 +1,7 @@
 package com.utn.teamA;
 
 import java.util.List;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class Plato {
@@ -10,6 +11,8 @@ public class Plato {
     private List<Ingrediente> listaDeIngredites;
     private String descripcion;
     private TipoPlato tipo;
+    //TODO el plato no tiene precio ???
+    //private double precio;
 
     //region constructores
 
@@ -79,8 +82,68 @@ public class Plato {
                 Color.ANSI_CYAN + "Tipo: " + Color.ANSI_GREEN + tipo + "\n" +
                 Color.ANSI_CYAN + "Nombre:" + Color.ANSI_GREEN + nombre + "\n" +
                 Color.ANSI_CYAN + "Descripcion: " + Color.ANSI_GREEN + descripcion + "\n\n" +
-                Color.ANSI_CYAN + "<<<<< Ingredientes >>>>> " + Color.ANSI_GREEN + "\n" +
-                listaDeIngredites + "\n" +
+                //TODO buscar y agregar ingredientes al plato
+                //Color.ANSI_CYAN + "<<<<< Ingredientes >>>>> " + Color.ANSI_GREEN + "\n" +
+                //listaDeIngredites + "\n" +
                 Color.ANSI_RESET + "\n";
+    }
+
+    public void crear(){
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println(
+                Color.ANSI_BLUE +
+                        "\n\n" +
+                        " ----------------------------------------\n" +
+                        "| \t\t N U E V O    P L A T O \t\t |\n" +
+                        " ----------------------------------------" +
+                        " \n\n" +
+                        Color.ANSI_RESET);
+
+        int opcion = 0;
+        boolean continuar = false;
+
+        do{
+
+            System.out.println(Color.ANSI_CYAN + "Ingrese tipo:" + Color.ANSI_RESET);
+            System.out.println(Color.ANSI_GREEN + " 1 " + Color.ANSI_RESET + " Entrada");
+            System.out.println(Color.ANSI_GREEN + " 2 " + Color.ANSI_RESET + " Principal");
+            System.out.println(Color.ANSI_GREEN + " 3 " + Color.ANSI_RESET + " Postre");
+
+            opcion = Helpers.validarInt();
+            continuar = false;
+
+
+            switch (opcion){
+                case 1:
+                    this.tipo = TipoPlato.ENTRADA;
+                    break;
+                case 2:
+                    this.tipo = TipoPlato.PRINCIPAL;
+                    break;
+                case 3:
+                    this.tipo = TipoPlato.POSTRE;
+                    break;
+                default:
+                    continuar = true;
+                    System.out.println(
+                            Color.ANSI_RED +
+                                    "\n\t [[ " + opcion + " ]] NO ES UNA OPCION VALIDA \n" +
+                                    Color.ANSI_RESET
+                    );
+                    break;
+            }
+        }while (continuar);
+
+        System.out.println(Color.ANSI_CYAN + "Ingrese nombre:" + Color.ANSI_RESET);
+        this.nombre =  Helpers.nextLine();
+
+        System.out.println(Color.ANSI_CYAN + "Ingrese Descripcion:" + Color.ANSI_RESET);
+        this.descripcion =  Helpers.nextLine();
+
+
+        System.out.println( this );
+
     }
 }
