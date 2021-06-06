fc901427cf82ad8f5fa7ecfac71b72834df6acd9
Jose Alvarez U <jsemalvarez@gmail.com>
2021-06-06 17:49:34 -0300

    menu para CRUDL de la clase Menu

--- src/main/java/com/utn/teamA/EmpresaAux.java
+++ src/main/java/com/utn/teamA/EmpresaAux.java
@@ -9,18 +9,21 @@ public class EmpresaAux {
     //TODO ver donde va la lista de ingredientes Map o List
     private List<Ingrediente> listaDeIngredites = new ArrayList<>();
     private List<Plato> listaDePlatos = new ArrayList<>();
+    private List<Menu> listaDeMenus = new ArrayList<>();
 
     public void init(){
 
         //ingredienteMenu();
-        platoMenu();
+        //platoMenu();
 
         //listaDeIngredites.add(new Ingrediente("tomate","tomate",50,"proveedor"));
         //listaDeIngredites.add(new Ingrediente("papa","papa",50,"proveedor"));
 
         //Plato plato1 = new Plato("Ensalada",listaDeIngredites,"ensalada",TipoPlato.ENTRADA);
-
         //System.out.println(plato1);
+
+        //Menu menu1 = new Menu();
+        //menu1.crear();
 
     }
 
@@ -195,6 +198,94 @@ public class EmpresaAux {
         nuevoPlato.crear();
 
         listaDePlatos.add(nuevoPlato);
+    }
+
+    //endregion
+
+
+    public void menuMenu(){
+        int opcion = 0;
+        boolean continuar = true;
+
+        do{
+            System.out.println(
+                    Color.ANSI_BLUE +
+                            "\n\n" +
+                            " -----------------------------\n" +
+                            "| \t\t M E N U  \t\t |\n" +
+                            " -----------------------------" +
+                            " \n\n" +
+                            Color.ANSI_RESET);
+            System.out.println(Color.ANSI_YELLOW + "\t Ingrese una opcion:" + Color.ANSI_RESET);
+            System.out.println(Color.ANSI_GREEN + " 1 " + Color.ANSI_RESET + " Nuevo Menu");
+            System.out.println(Color.ANSI_GREEN + " 2 " + Color.ANSI_RESET + " Editar Menu");
+            System.out.println(Color.ANSI_GREEN + " 3 " + Color.ANSI_RESET + " Eliminar Menu");
+            System.out.println(Color.ANSI_GREEN + " 4 " + Color.ANSI_RESET + " Listar Menus");
+            System.out.println(Color.ANSI_RED + " 0 " + Color.ANSI_RESET + " Volver");
+
+            opcion = Helpers.validarInt();
+
+            switch (opcion){
+                case 0:
+                    continuar = false;
+                    break;
+                case 1:
+                    agregarMenuView();
+                    break;
+                case 2:
+                    System.out.println(Color.ANSI_BLACK + "Seccion en construccion" + Color.ANSI_RESET);
+                    break;
+                case 3:
+                    System.out.println(Color.ANSI_BLACK + "Seccion en construccion" + Color.ANSI_RESET);
+                    break;
+                case 4:
+                    for ( Menu menu : listaDeMenus ) {
+                        System.out.println(menu);
+                    }
+                    break;
+                default:
+                    System.out.println(
+                            Color.ANSI_RED +
+                                    "\n\t [[ " + opcion + " ]] NO ES UNA OPCION VALIDA \n" +
+                                    Color.ANSI_RESET
+                    );
+                    break;
+
+            }
+
+        } while( continuar );
+    }
+
+
+    //region agregar plato
+    public void agregarMenuView(){
+
+        char unElementoMas = 's';
+
+        while ( unElementoMas == 's' ){
+            agregarMenu();
+
+            System.out.print(
+                    Color.ANSI_YELLOW + " \n\n \t\t Desea agregar otro menu? " +
+                            Color.ANSI_YELLOW + "[" +
+                            Color.ANSI_GREEN + " S " +
+                            Color.ANSI_YELLOW + "/" +
+                            Color.ANSI_RED + " N " +
+                            Color.ANSI_YELLOW + "]" + Color.ANSI_RESET);
+            unElementoMas = Helpers.charAt0();
+
+
+        }
+
+    }
+
+    public void agregarMenu(){
+
+        Menu nuevoMenu = new Menu();
+
+        nuevoMenu.crear();
+
+        listaDeMenus.add(nuevoMenu);
     }
 
     //endregion
