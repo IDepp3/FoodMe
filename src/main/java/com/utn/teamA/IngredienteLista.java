e01f50389dc3431e151b716caf784153ddfcd369
Jose Alvarez U <jsemalvarez@gmail.com>
2021-06-12 22:57:25 -0300

    se incorpora funcionalidad para ediatar un ingrediente

--- src/main/java/com/utn/teamA/IngredienteLista.java
+++ src/main/java/com/utn/teamA/IngredienteLista.java
@@ -63,19 +63,13 @@ public class IngredienteLista {
                     agregarIngredienteView();
                     break;
                 case 2:
-                    System.out.println(Color.ANSI_BLACK + "Seccion en construccion" + Color.ANSI_RESET);
+                    this.editarIngrediete();
                     break;
                 case 3:
                     this.eliminarIngrediete();
                     break;
                 case 4:
-                    listaDeIngredites = ingredientePersistencia.obtenerRegistros();
-                    for ( Ingrediente ingrediente : listaDeIngredites ) {
-                        if (ingrediente.getEstado()){
-                            System.out.println(ingrediente);
-                        }
-
-                    }
+                    this.mostrarIngredientesActivos();
                     break;
                 default:
                     System.out.println(
@@ -100,6 +94,104 @@ public class IngredienteLista {
 
     }
 
+    public void editarIngrediete(){
+        listaDeIngredites = ingredientePersistencia.obtenerRegistros();
+        int i = 0;
+
+        System.out.println(Color.ANSI_YELLOW + "\t Ingrese el codigo del ingrediente a editar" + Color.ANSI_RESET);
+
+        for ( Ingrediente ing : listaDeIngredites ) {
+
+            if( ing.getEstado() ){
+                System.out.println( Color.ANSI_YELLOW + "[ " + i  + " ] " + Color.ANSI_RESET + ing.getNombre() );
+            }
+            i++;
+        }
+
+        System.out.println("");
+        int codigo = Helpers.validarInt();
+
+        if (codigo < 0 || codigo > listaDeIngredites.size() || !listaDeIngredites.get(codigo).getEstado()){
+            System.out.println("Opcion invalida");
+        }else {
+            Ingrediente ing = listaDeIngredites.get(codigo);
+
+            ing = editarIngredienteVista(ing);
+
+            if( ing.getId() != null ){
+                System.out.println(ing);
+                ingredientePersistencia.actualizarRegistro(ing);
+            }
+
+        }
+
+    }
+
+    public Ingrediente editarIngredienteVista( Ingrediente ing){
+        int opcion = 0;
+        boolean continuar = true;
+
+        do{
+
+            System.out.println(
+                    Color.ANSI_BLUE +
+                            "\n\n" +
+                            " ------------------------------------------------\n" +
+                            "| \t\t E D I T A R    I N G R E D I E N T E \t\t |\n" +
+                            " ------------------------------------------------" +
+                            " \n\n" +
+                            Color.ANSI_RESET);
+
+            System.out.println(ing);
+
+            System.out.println(Color.ANSI_YELLOW + "\t Ingrese una opcion:" + Color.ANSI_RESET);
+            System.out.println(Color.ANSI_GREEN + " 1  Guardar Cambios" + Color.ANSI_RESET);
+            System.out.println(Color.ANSI_GREEN + " 2 " + Color.ANSI_RESET + " Editar Nombre");
+            System.out.println(Color.ANSI_GREEN + " 3 " + Color.ANSI_RESET + " Editar Descripcion");
+            System.out.println(Color.ANSI_GREEN + " 4 " + Color.ANSI_RESET + " Editar Precio");
+            System.out.println(Color.ANSI_RED + " 0 " + Color.ANSI_RESET + " Cancelar");
+
+            opcion = Helpers.validarInt();
+
+            switch (opcion){
+                case 0:
+                    continuar = false;
+                    ing.setId(null);
+                    break;
+                case 1:
+                    continuar = false;
+                    break;
+                case 2:
+                    System.out.print(Color.ANSI_CYAN + "Ingrese nombre: " + Color.ANSI_RESET);
+                    System.out.println(Color.ANSI_YELLOW + "< " + ing.getNombre() + " >" + Color.ANSI_RESET);
+                    ing.setNombre( Helpers.nextLine() );
+                    break;
+                case 3:
+                    System.out.print(Color.ANSI_CYAN + "Ingrese Descripcion: " + Color.ANSI_RESET);
+                    System.out.println(Color.ANSI_YELLOW + "< " + ing.getDescripcion() + " >" + Color.ANSI_RESET);
+                    ing.setDescripcion( Helpers.nextLine() );
+                    break;
+                case 4:
+                    System.out.print(Color.ANSI_CYAN + "Ingrese precio: " + Color.ANSI_RESET);
+                    System.out.println(Color.ANSI_YELLOW + "< " + ing.getPrecio() + " >" + Color.ANSI_RESET);
+                    ing.setPrecio( Helpers.validarDouble() );
+                    break;
+                default:
+                    System.out.println(
+                            Color.ANSI_RED +
+                                    "\n\t [[ " + opcion + " ]] NO ES UNA OPCION VALIDA \n" +
+                                    Color.ANSI_RESET
+                    );
+                    break;
+            }
+
+        } while( continuar );
+
+        return ing;
+    }
+
+
+
     public void eliminarIngrediete(){
         listaDeIngredites = ingredientePersistencia.obtenerRegistros();
         int i = 0;
@@ -123,5 +215,14 @@ public class IngredienteLista {
             ingredientePersistencia.borrarRegistro(ing);
         }
 
+    }
+
+    public  void mostrarIngredientesActivos(){
+        listaDeIngredites = ingredientePersistencia.obtenerRegistros();
+        for ( Ingrediente ingrediente : listaDeIngredites ) {
+            if (ingrediente.getEstado()){
+                System.out.println(ingrediente);
+            }
+        }
     }
 }
